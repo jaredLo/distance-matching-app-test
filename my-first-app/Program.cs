317e63894a6cf339e3c9532f299756a279827222
Gerard <gerardlo.psd@gmail.com>
2023-01-24 20:46:16 +0800

    Commit again cause I forgot to save Program.cs

--- my-first-app/Program.cs
+++ my-first-app/Program.cs
@@ -4,29 +4,6 @@ using my_first_app.Tests;
 
 namespace FirstApp
 {
-
-    class Cat : Animal
-    {
-
-
-        public void Meow()
-        {
-            Console.WriteLine("Meow Meow!");
-        }
-
-        public void aboutMe(string genderName)
-        {
-
-            string CatName = Name ?? "Booboo";
-            Console.WriteLine($"Name: {CatName}");
-            Console.WriteLine($"Age: {Age}");
-            Console.WriteLine($"Gender: {genderName}");
-        }
-
-    }
-
-
-
     class Program
     {
         static void Main()
